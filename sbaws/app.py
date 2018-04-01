20c6aa5984f583433ad1b9aff67cd1a2f79b10a9
Chris Allison <chris.charles.allison@gmail.com>
2018-04-01 10:55:36 +0100

    initial chalice app

--- sbaws/app.py
+++ sbaws/app.py
@@ -3,27 +3,6 @@ from chalice import Chalice
 app = Chalice(app_name='sbaws')
 
 
-@app.route('/')
+@app.route('/', methods=['POST'], content_types=['application/x-www-form-urlencoded'])
 def index():
-    return {'hello': 'world'}
-
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+    return {'starback': 'lambda'}
